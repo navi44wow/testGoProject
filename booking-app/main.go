934cf82f41352693a89da1ba98066157c8a94b63
navi44wow <ivan.belev69@gmail.com>
2025-03-27 23:35:25 +0200

    initial commit 	new file:   booking-app/booking-app.exe 	new file:   booking-app/main.go

---
+++ booking-app/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+
+	fmt.Println("Welcome to ", conferenceName, " booking app!")
+	fmt.Println("See your tickets!")
+
+}
