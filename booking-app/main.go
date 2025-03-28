d37f4dd1d45a6fa3d4ca77ccc215181be382f3da
ivan-belev <ivan.belev@sumup.com>
2025-03-28 10:11:02 +0200

    added formatting 	modified:   booking-app/main.go

--- booking-app/main.go
+++ booking-app/main.go
@@ -6,8 +6,10 @@ func main() {
 
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
+	var remainingTickets = 50
 
-	fmt.Println("Welcome to ", conferenceName, " booking app!")
-	fmt.Println("See your tickets!")
+	fmt.Printf("Welcome to   %v booking application\n", conferenceName)
+	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 
 }
