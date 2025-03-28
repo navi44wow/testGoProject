1efa03c426a257bf847942e3b28f7e7323eb47ea
ivan-belev <ivan.belev@sumup.com>
2025-03-28 18:14:04 +0200

    other changes: 	modified:   booking-app/main.go

--- booking-app/main.go
+++ booking-app/main.go
@@ -8,8 +8,19 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T\n", conferenceName, remainingTickets)
+
 	fmt.Printf("Welcome to   %v booking application\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTicket int
+	//ask user for their name
+
+	userName = "Tom"
+
+	userTicket = 2
+
+	fmt.Printf("USER %v BOOKED %v tickets.\n", userName, userTicket)
 }
